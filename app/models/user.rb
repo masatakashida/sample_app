c7f5397f840411147599f86490cde325dea68602
masataka <ar.tec.masa@gmail.com>
2018-01-17 16:03:56 +0900

    存在と長さの検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
   #validates(:name, presence: true)
-  validates :email, presence: true
+  validates :email, presence: true, length: { maximum: 255 }
 end
