71ab77065f45d9edd8e3eaae5274044c2248cc35
masataka <ar.tec.masa@gmail.com>
2018-01-17 13:17:39 +0900

    signupページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   # rootルーティングを追加したことで、root_urlというRailsヘルパー
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
- 
+  get '/signup', to: 'users#new'
 end
