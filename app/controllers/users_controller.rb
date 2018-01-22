45cd138ee7850dee3c449c9db903121d60d5a605
masataka <ar.tec.masa@gmail.com>
2018-01-23 00:03:29 +0900

    debugger

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    #debugger
+    # その行の操作をしている時に止まる。その瞬間にどんな状態かわかる
   end
 
   def new
+    debugger
   end
 end
