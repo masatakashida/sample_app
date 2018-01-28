995d908d68b4c64a580bf49a08af383fef96724d
masataka <ar.tec.masa@gmail.com>
2018-01-29 01:52:13 +0900

    ユーザー登録・ログイン・ログアウト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,21 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    #if user && user.authenticate(params[:session][:password])
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       # エラーメッセージを作成する
+      #flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
