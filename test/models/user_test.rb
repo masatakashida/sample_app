666ff62c5b24dd83b42cd169f3f7c6200086f9ca
masataka <ar.tec.masa@gmail.com>
2018-02-01 23:55:31 +0900

    二つの目立たないバグ

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -74,4 +74,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "autheticated? should return false for a user with nil disget" do
+    assert_not @user.authenticated?('')
+  end
 end
